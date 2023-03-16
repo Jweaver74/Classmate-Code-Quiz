e580f56afade315cd347b54e3953587372d46984
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-03-16 10:26:36 -0500

    added answer select if-else function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@
 
 var timeLeft = 60;
 var timerId;
-var temerEl = document.getElementById("timer");
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-btn");
+var answerButtonEl = document.getElementById("answer-btn");
 var startContainerEl = document.getElementById("start-container");
 var questionContainerEl = document.getElementById("question-container");
+var checkAnswerEl = document.getElementById("check-answer");
 var shuffledQuestions, currentQuestionIndex;
 
 
@@ -35,3 +38,49 @@ function startGame() {
     setNextQuestion();
 
 };
+
+function setNextQuestion(){
+    resetstate();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+
+function showQuestion(question){
+questionContainerEl.innerText = question.question;
+question.answers.forEach(answer =>{
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct){
+        button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonEl.appendChild(button);
+})
+};
+
+function resetstate(){
+    nextButton.classList.add("hide");
+    checkAnswerEl.classList.add("hide");
+    while(answerButtonEl.firstChild){
+        answerButtonEl.removeChild;
+        (answerButtonEl.firstChild);
+    }
+};
+
+function selectAnswer(e){
+    var selectButton = e.target;
+    var correct = selectButton.dataset.correct;
+    checkAnswerEl.classList.remove("hide");
+    
+    if(correct){
+        checkAnswerEl.innerHTML = "You are correct";
+    }else {
+        checkAnswerEl.innerHTML = "That was incorrect";}
+
+        if (timeLeft <= 10){
+            timeLeft = 0;
+        }else{
+            timeLeft -=10;
+        }   
+}
