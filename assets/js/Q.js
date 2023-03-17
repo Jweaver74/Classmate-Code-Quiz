832fec6f046ffc15246085b63486f6bc11ca802c
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-03-17 14:34:53 -0500

    Had BCS resoure helpign fix alot of errors. Still working on it

---
+++ assets/js/Q.js
@@ -0,0 +1,21 @@
+var questions = [
+    {
+    question: "Commonly used data types DO NOT include,",
+    answers:[
+    {text:"strings", correct: false},
+    {text: "booleans", correct: false},
+    {text: "alerts", correct: true},
+    {text: "numbers", correct: false}
+]
+},
+{
+    question: "String values must be enclosed ____ when being assigned to variables.",
+    answers:[
+    {text: "commas", correct: false},
+    {text: "curly brtackets", correct: false},
+    {text: "paraenthesis", correct: false},
+    {text: "quotes", correct: true},
+    ]
+},
+
+]
