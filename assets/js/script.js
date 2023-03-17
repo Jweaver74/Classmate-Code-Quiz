78250fdbcd800c13f961395c4cd89d1a9227dd87
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-03-17 09:16:14 -0500

    added highscore function with console log.  Corrected missing score button in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,13 @@ var timerId;
 var timerEl = document.getElementById("timer");
 var startButton = document.getElementById("start-btn");
 var nextButton = document.getElementById("next-btn");
-var answerButtonEl = document.getElementById("answer-btn");
+var answerButtonsEl = document.getElementById("answer-btn");
 var startContainerEl = document.getElementById("start-container");
 var questionContainerEl = document.getElementById("question-container");
 var checkAnswerEl = document.getElementById("check-answer");
+var initialsField = document.getElementById("player-name");
+var scoreField = document.getElementById("player-score");
+var scores = JSON.parse(localStorage.getItem("scores")) || [];
 var shuffledQuestions, currentQuestionIndex;
 
 
@@ -86,7 +89,7 @@ function selectAnswer(e){
         }   
 }
 
-Array.from(answerButtonEl.children).forEach(button => {
+Array.from(answerButtonsEl.children).forEach(button => {
     setStatusClass(button, button.dataset.correct);
 })
 
@@ -97,12 +100,13 @@ if (shuffledQuestions.length > currentQuestionIndex + 1){
     saveScore();
 }
 
-function setStatusClass(element, correct);
+function setStatusClass(element, correct) {
 clearStatusClass(element);
 if (correct){
     element.classList.add("correct");
 }else{
     element.classList.add("wrong")
+}
 };
 
 function saveScore(){
@@ -114,3 +118,49 @@ function saveScore(){
         document.getElementById("your-score").textContent = "Final Score" + timeLeft;
     },2000)
 };
+
+var loadScores = function(){
+    if (!savedScores){
+        return false;
+    }
+    savedScores = JSON.parse(savedScores);
+    var initials = document.querySelector("#initials-field").value;
+    var newScore = {
+        score: timeLeft, initials: initials
+    }
+    savedScores.push(newScore);
+    console.log(savedScores);
+
+    savedScores.forEach(score => {
+        initialsField.innerText = score.initials;
+        scoreField.innerText = score.score;
+    })
+};
+
+function showHighScores(initials){
+    document.getElementById("highscores").classList.remove("hide");
+    document.getElementById("score-container").classList.add("hide");
+    startContainerEl.classList.add("hide");
+    questionContainerEl.classList.add("hide");
+    if(typeof intitials == "string"){
+        var score = {
+            initials, timeLeft
+        }
+        score.push(score)
+    }
+
+    var highScoreEl = document.getElementById("highscore");
+    highScoreEl.innerHTML = "";
+    for (index = 0; i < scores.length; i++) {
+       var div1 = document.createElement("div");
+       div1.setAttribute("class", "name-div");
+       div1.innerText = scores[i].initials;
+       var div2 = document.createElement("div");
+       div2.setAttribute("class", "score-div");
+       div2.innerText = scores[i].timeLeft;
+
+       highScoreEl.appendChild(div1);
+       highScoreEl.appendChild(div2);
+    }
+    localStorage.setItem("scores", JSON.stringify(scores));
+}:
