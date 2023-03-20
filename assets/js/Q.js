8b65d1aba12bfb6c2908f0a764a1255e644454a6
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-03-20 08:24:19 -0500

    added question and answers

--- assets/js/Q.js
+++ assets/js/Q.js
@@ -18,4 +18,33 @@ var questions = [
     ]
 },
 
+{
+    question: "Which of the following function of Array object calls for each element in the Array?",
+    answers:[
+        {text: "foreach ()", correct: true},
+        {text: "contact ()", correct: false},
+        {text: "filter ()", correct: false},
+        {text: "split ()", correct: false},
+    ]
+},
+
+{
+    question: "What does CSS stand for?",
+    answers:[
+        {text: "Common Status Sheet", correct: false},
+        {text: "Cascading style sheet", correct: true},
+        {text: "Clear Staged  Selectors", correct: false},
+        {text: "Counter Selector Sheet", correct: false},
+    ]
+},
+
+{
+    question: "Which HTML element is used to define a paragraph?",
+    answers:[
+        {text: "<div>", correct: false},
+        {text: "<span>", corrct: false},
+        {text: "<p>", correct: true},
+        {text: "<h1>", correct: false},
+    ]
+},
 ]
